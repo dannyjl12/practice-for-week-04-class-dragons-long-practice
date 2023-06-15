3bcfac777bb4ec08df0bafc562cfe8e97e532af7
danny-lowton <dannylowton@gmail.com>
2023-06-15 16:21:34 -0400

    completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,22 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let dragonNames = [];
+    for (let dragon of dragons) {
+      dragonNames.push(dragon.name)
+    }
+    return dragonNames;
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,22 @@
-// Your code here
+const isEsModule = require("sinon/lib/sinon/util/core/is-es-module");
+const Dragon = require("./dragon")
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals, friend) {
+        super (name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        for (let lifeGoal of this.lifeGoals) {
+            console.log(`${this.name} likes to ${lifeGoal}`)
+        }
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+module.exports = FriendlyDragon;
